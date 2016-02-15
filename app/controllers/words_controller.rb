d2f3507ab8e79738244fd517d9f5823342591124
codegracie <grace.m.sojor@framgia.com>
2016-02-15 17:39:19 +0800

    fix words index and controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,15 +1,13 @@
 class WordsController < ApplicationController
  
   def index
-    if params[:search]
-      @words = Word.paginate(page: params[:page]).search(params[:search]).order("created_at DESC")
-    else
-      @words = Word.paginate page: params[:page], per_page: 20
-    end
-
     respond_to do |format|
-     @categories = Category.all
-     #@words = @words.paginate page: params[:page], per_page: 10
+      @categories = Category.all
+      if params[:search]
+        @words = Word.paginate(page: params[:page]).search(params[:search]).order("created_at DESC")
+      else
+        @words = Word.paginate page: params[:page], per_page: 20
+      end
        
        format.html
        format.json
@@ -17,32 +15,12 @@ class WordsController < ApplicationController
           render json: @words, status: :ok
        end
      end
-
   end
-
 
   def show
     @word = Word.find params[:id]
     @word_answers = @word.answers
   end
-
-
-#respond_to :html, :json
- #  def index
- #    respond_to do |format|
- #      @categories = Category.all
- #      #@words = @words.paginate page: params[:page], per_page: 10
-       
- #      format.html
- #      format.json
- #      format.json do
- #         render json: @words, status: :ok
- #      end
- #    end
- #  end
-
-
-
 
 end
 
